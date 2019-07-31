562b53a50d750f8fb28a386d06da49c8d41dd333
Stefano Cecere <stefano.cecere@krur.com>
2019-08-01 00:38:42 +0200

    disable bootstrap check

--- Assets/_app/_scripts/_Core/BootstrapScene.cs
+++ Assets/_app/_scripts/_Core/BootstrapScene.cs
@@ -18,17 +18,17 @@ namespace Antura.Scenes
             Debug.Log("BootstrapScene: Start()");
             // GlobalUI.ShowPauseMenu(false);
 
-            if (AppManager.I.AppSettingsManager.IsAppJustUpdatedFromOldVersion()) {
-                Debug.Log("BootstrapScene: Updating from Old version");
-                if (AppManager.I.AppSettings.SavedPlayers != null) {
-                    AppManager.I.PlayerProfileManager.DeleteAllPlayers();
-                    PanelAppUpdate.Init();
-                } else {
-                    GoToHomeScene();
-                }
-            } else {
-                GoToHomeScene();
-            }
+            //if (AppManager.I.AppSettingsManager.IsAppJustUpdatedFromOldVersion()) {
+            //    Debug.Log("BootstrapScene: Updating from Old version");
+            //    if (AppManager.I.AppSettings.SavedPlayers != null) {
+            //        AppManager.I.PlayerProfileManager.DeleteAllPlayers();
+            //        PanelAppUpdate.Init();
+            //    } else {
+            //        GoToHomeScene();
+            //    }
+            //} else {
+            GoToHomeScene();
+            //}
         }
 
         private void GoToHomeScene()
